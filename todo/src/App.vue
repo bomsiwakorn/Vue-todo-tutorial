a24831341e9191de95b3d140dd4b6697692c2cbe
Bomsiwakorn <bomsiwakorns@gmail.com>
2020-07-15 09:14:55 +0700

    Finish todo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="item in sortedItem" :key="item.time">
+        <button @click="done(item.time)">Done</button>
+        {{item.text | capitalized}}
+      </li>
+    </ul>
+    <form action="" @submit.enter.prevent="save()">
+      <input type="text" v-model="inputText">
+      <button type="submit">Save</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      inputText: '',
+      items: [],
+    }
+  },
+  filters: {
+    capitalized (val) {
+      return val.toUpperCase()
+    }
+  },
+  mounted () {
+    this.items = JSON.parse(localStorage['todoItems'])
+  },
+  computed: {
+    sortedItem () {
+      return this.items.sort((a,b) => {
+        return b.time - a.time
+      }).filter(ele => {
+        return ele.completed === false
+      })
+    }
+  },
+  methods: {
+    done (id) {
+      this.items = this.items.map(ele => {
+        if (id === ele.time) {
+          ele.completed = true
+        }
+        return ele
+      })
+      localStorage['todoItems'] = JSON.stringify(this.items)
+    },
+    save () {
+      this.items.push({
+        text: this.inputText,
+        time: Date.now(),
+        completed: false
+      })
+      localStorage['todoItems'] = JSON.stringify(this.items)
+    }
   }
 }
 </script>
